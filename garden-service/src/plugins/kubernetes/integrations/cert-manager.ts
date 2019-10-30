deecd5554b2095632bc9c5244e75faf62be23e5f
Emanuele Libralato <e.libralato@gmail.com>
2019-10-30 15:06:54 +0100

    chore: added license to cert-manager

--- garden-service/src/plugins/kubernetes/integrations/cert-manager.ts
+++ garden-service/src/plugins/kubernetes/integrations/cert-manager.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2018 Garden Technologies, Inc. <info@garden.io>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import {
   KubernetesProvider,
   IngressTlsCertificate,
@@ -79,6 +87,7 @@ export interface GetCertificateParams {
   tlsCertificate: IngressTlsCertificate
   issuerName: string
 }
+
 /**
  * Helper function for generating a cert-manager Certificate
  *
